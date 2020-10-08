651a3327ce9ba2fc9f0bd249a801871028decb09
nikolay7424 <nikolay7424@gmail.com>
2020-10-08 21:14:06 +0300

    initial commit

---
+++ edge_ledger/js/main.js
@@ -0,0 +1,34 @@
+// google maps
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+//sticky menu background
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+// jquery
+$('#navbar a, .btn').on('click', function(event){
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
